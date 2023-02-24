f47a48c4d92ea30bd84946aa191183b2617846f0
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-02-24 19:41:45 -0300

    Criação do app para mobile

---
+++ mobile/src/mocks/categories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    _id: '63f8e13a0bcabb812df1db67',
+    name: 'Pizza',
+    icon: '🍕',
+  },
+  {
+    _id: '63f8e14d0bcabb812df1db69',
+    name: 'Bebidas',
+    icon: '🍻',
+  },
+  {
+    _id: '63f8e1560bcabb812df1db6b',
+    name: 'Burgers',
+    icon: '🍔',
+  },
+  {
+    _id: '63f8e15d0bcabb812df1db6d',
+    name: 'Promoções',
+    icon: '🏷',
+  }
+];
---
+++ mobile/src/mocks/products.ts
@@ -0,0 +1,39 @@
+export const products = [
+  {
+    _id: '63f8e4cc0bcabb812df1db73',
+    name: 'Pizza quatro queijos',
+    description: 'Deliciosa pizza quatro queijos com borda simples',
+    imagePath: '1677255884204-quatro-queijos.png',
+    price: 40,
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '63f8e4cc0bcabb812df1db74'
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '63f8e4cc0bcabb812df1db75'
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '63f8e4cc0bcabb812df1db76'
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '63f8e4cc0bcabb812df1db77'
+      }
+    ],
+  },
+  {
+    _id: '63f8e67c0bcabb812df1db7f',
+    name: 'Coca cola',
+    description: 'Coca cola 350ml. Bem fria, te faz disfrutar cada momento da vida',
+    imagePath: '1677256316671-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+];
